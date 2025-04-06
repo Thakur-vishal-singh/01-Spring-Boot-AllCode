5bc5018a9c8a7d3674ebda0107b621dbca1f0db1
thakur vishal singh <vishalrajput0246@gmail.com>
2025-04-06 07:46:32 -0400

    Added 01-Spring-final-demo-work

---
+++ 01-Sping-final-demo-work/src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	public void helloWorld() {
+		System.out.println("hello guys how are you");
+	}
+	
+	
+}
